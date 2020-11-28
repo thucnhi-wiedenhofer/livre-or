c3c1c62b49c32b5aeb570ed6bf97096b98ca1534
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2020-11-28 10:41:22 +0100

    changement image banner index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if(isset($_POST['session_fin']))
 <body>
     
     <header class="page-header" >
-        <div id="banner"></div>       
+        <div id="banner_index"></div>       
                     
     </header>
     
